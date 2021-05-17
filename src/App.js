54e1a71da6c4ab1abdfa2db489c93f3ce67174b7
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-16 21:39:17 -0500

    new commit fresh for 17 may

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import Chat from './Chat';
-import Sidebar from './Sidebar';
+
 
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__body">
-        <Sidebar />
-      <Chat />
-      </div>
+    <div className="App">
+      
       
     </div>
   );
